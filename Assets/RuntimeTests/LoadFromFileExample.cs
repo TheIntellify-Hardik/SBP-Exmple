f445d9dd55c7ea006071065093b5419bc51a6d95
Hardik Shah <hardik.s@theintellify.com>
2020-07-21 10:39:12 +0530

    init

---
+++ Assets/RuntimeTests/LoadFromFileExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadFromFileExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var myLoadedAssetBundle
+                = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "prefabs"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+       // var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Cube");
+        var prefab  = Instantiate(myLoadedAssetBundle.mainAsset) as GameObject;
+        Instantiate(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
